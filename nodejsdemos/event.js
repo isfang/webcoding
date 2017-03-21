cc03168d66ca50c8017b3bbd7359a131ff8bca66
tuoni <tuoni@mogujie.com>
2017-03-21 11:02:58 +0800

    ci

---
+++ nodejsdemos/event.js
@@ -0,0 +1,18 @@
+var events = require('events');
+
+var eventEmitter = new events.EventEmitter();
+
+var connectHandler = function connected() {
+	console.log('connect success');
+	eventEmitter.emit('data_received');
+}
+
+eventEmitter.on('connection', connectHandler);
+
+eventEmitter.on('data_received', function(){
+	console.log('receive succes');
+});
+
+eventEmitter.emit('connection');
+
+console.log('execute end');
---
+++ nodejsdemos/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function(request, response){
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write('dddd');
+	response.end();
+}).listen(8888);
+console.log('running at http://127.0.0.1:8888');
